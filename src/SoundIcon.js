8ce728b438a348f99ca824d64bc1cfcc4d06f800
Yurko Turskiy <guandjoy@gmail.com>
2020-03-25 22:46:31 +0200

    Update SoundIcon.js

--- src/SoundIcon.js
+++ src/SoundIcon.js
@@ -8,102 +8,107 @@ import React, {
 import "./styles.css";
 
 const init = power => ({
-  isPlaying: false,
-  repeatCount: power ? "indefinite" : "0",
+  animate: power,
+  power,
+  repeatCount: "indefinite",
   numOfCycles: 0,
-  duration: "680ms"
+  duration: "600ms"
 });
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "ON":
-      return { ...state, repeatCount: "indefinite" };
+      return { ...state, animate: true, power: true };
     case "OFF":
-      return { ...state, repeatCount: "0" };
+      return { ...state, power: false };
     case "REPEAT":
-      return { ...state, numOfCycles: state.numOfCycles + 1 };
+      return { ...state, numOfCycles: state.numOfCycles + 1, animate: state.power };
     default:
       return state;
   }
 };
 
-const createAction = dispatch => type => payload => dispatch({ type, payload });
+const createDispatcher = dispatch => type => payload => dispatch({ type, payload });
 
 export default function App({ power }) {
   const [state, dispatch] = useReducer(reducer, power, init);
-  const repeatAction = createAction(dispatch);
+  // actions
+  const dispatcher = createDispatcher(dispatch)
+  const repeatAction = dispatcher("REPEAT");
+  const onAction = dispatcher("ON")
+  const offAction = dispatcher("OFF")
+
+  // refs
   const animateRef = useRef(null);
-  const [switcher, setSwitcher] = useState(false);
-  const [repeatCount, setRepeatCount] = useState(power ? "indefinite" : "0");
-  const [repeatCycles, setRepeatCycles] = useState(0);
-  const [duration] = useState("680ms");
+
   const animatePattern = {
     calcMode: "linear",
     attributeName: "d",
     dur: state.duration,
     repeatCount: state.repeatCount
   };
-  console.log("animatePattern", animatePattern);
+
+  useEffect(() => {
+    power ? onAction() : offAction()
+  }, [power])
 
   const onRepeat = useCallback(e => {
-    console.log("repeat event", e);
-    setRepeatCycles(repeatCycles => repeatCycles + 1);
-  }, []);
+    repeatAction(e)
+  }, [repeatAction]);
 
   // Set animation repeat event
   useEffect(() => {
     const animateElement = animateRef.current;
-    animateElement.addEventListener("repeatEvent", onRepeat);
+    animateElement && animateElement.addEventListener("repeatEvent", onRepeat);
     return () => {
-      animateElement.removeEventListener("repeatEvent", onRepeat);
+      animateElement && animateElement.removeEventListener("repeatEvent", onRepeat);
     };
   });
-
-  // Handle power and repeatCount
-  useEffect(() => {
-    setRepeatCount(power ? "indefinite" : repeatCount);
-    console.log("power", power);
-  }, [power, repeatCount]);
-
-  // Handle repeat cycles
-  useEffect(() => {
-    console.log("repeat cycles", repeatCycles);
-  }, [repeatCycles, power]);
 
   return (
     <svg className="icon" viewBox="0 0 24 24">
       <path d="M4 10 L 4 14 Z">
-        <animate
+        {state.animate  && (
+          <animate
           ref={animateRef}
           {...animatePattern}
           values="M4 10 L4 14 Z; M4 4 L4 20 Z; M4 10 L4 14 Z"
         />
+        )}
       </path>
       <path d="M8 8 L 8 14 Z">
-        <animate
+        {state.animate && (
+          <animate
           {...animatePattern}
           keyTimes="0; 0.25; 0.75; 1"
           values="M8 7 L8 17 Z; M8 10 L8 15 Z; M8 4 L8 20 Z; M8 7 L8 17 Z"
         />
+        )}
       </path>
       <path d="M12 4 L12 20 Z">
+        {state.animate && (
         <animate
           {...animatePattern}
           values="M12 4 L12 20 Z; M12 10 L12 14 Z; M12 4 L12 20 Z"
         />
+        )}
       </path>
       <path d="M16 7 L16 17 Z">
+        {state.animate && (
         <animate
           {...animatePattern}
           keyTimes="0; 0.25; 0.75; 1"
           values="M16 7 L16 17 Z; M16 4 L16 20 Z; M16 10 L16 14 Z; M16 7 L16 17 Z"
         />
+        )}
       </path>
       <path d="M20 10 L20 14 Z">
+        {state.animate && (
         <animate
           {...animatePattern}
           values="M20 10 L20 14 Z; M20 4 L20 20 Z; M20 10 L20 14 Z"
         />
+        )}
       </path>
     </svg>
   );
