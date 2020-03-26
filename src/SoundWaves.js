e9af1d209031751604dc9d10ce522a5fdc3bceab
Yurko Turskiy <guandjoy@gmail.com>
2020-03-26 21:22:27 +0200

    Update SoundWaves duration to 1000-1600ms

--- src/SoundWaves.js
+++ src/SoundWaves.js
@@ -62,7 +62,7 @@ function SoundWaves(props) {
         <path key={i} d={blob.dValuesFrames[0]}>
           <animate
             attributeName="d"
-            dur={`${getRandomArbitrary(600, 1000)}ms`}
+            dur={`${getRandomArbitrary(1000, 1600)}ms`}
             repeatCount="indefinite"
             calcMode="linear"
             values={morphing(morphParams, pathParams).dValues}
@@ -71,7 +71,7 @@ function SoundWaves(props) {
             begin={props.shiftStep * i + "ms"}
             attributeName="stroke"
             values={animateColorValues}
-            dur={`${getRandomArbitrary(600, 1000)}ms`}
+            dur={`${getRandomArbitrary(1000, 1600)}ms`}
             repeatCount="indefinite"
           />
         </path>
