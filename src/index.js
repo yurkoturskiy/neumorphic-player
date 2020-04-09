e5a15f26f5eefc8043b371a0a5c974632c8d8a05
Yurko Turskiy <guandjoy@gmail.com>
2020-04-09 23:57:04 +0300

    Add CodeSandboxLogo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import CodeSandboxLogo from "./misc/CodeSandboxLogo";
 import Player from "./Player";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
+    {/* <CodeSandboxLogo /> */}
     <Player />
   </React.StrictMode>,
   rootElement
